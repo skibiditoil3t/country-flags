8dc27915bc512fb5e6d582ef9b9b31d7e83f4ddf
skibidimush <159740396+skibiditoil3t@users.noreply.github.com>
2026-03-16 12:14:55 +1300

    need to fix buttons and organise again

--- C_01_Flags_Gui_v1.py
+++ C_01_Flags_Gui_v1.py
@@ -2,15 +2,11 @@ from tkinter import *
 from functools import partial # To prevent unwanted windows
 
 class StartGame:
-    """
-    Initial Game interface (asks users how many rounds they
-    would like to play
-    """
+    """ Initial Game interface (asks users how many rounds they
+    would like to play) """
 
     def __init__(self):
-        """
-        Gets number of rounds from user
-        """
+        """ Gets number of rounds from user """
 
         self.start_frame = Frame(padx=10, pady=10)
         self.start_frame.grid()
@@ -19,14 +15,14 @@ class StartGame:
         intro_string = ("In each round you will have to guess the flag of a country (or capital if a harder difficulty is chosen). \n\nYour goal is to correctly guess the capital of a country and win the round."
                         "\n\nTo begin, please enter how many rounds you'd like to play and then choose your difficulty.")
 
-        # choose_string = "oops - please choose a whole number more than zero."
         choose_string = "How many rounds do you want to play?"
 
         # list of labels to be made (text | font | fg)
         start_labels_list = [
-            ["Colour Quest", ("Arial", 16, "bold"), None],
+            ["Country Flags", ("Arial", 16, "bold"), None],
             [intro_string, ("Arial", 12), None],
-            [choose_string, ("Arial", 12, "bold"), "#009900"]
+            [choose_string, ("Arial", 12, "bold"), "#009900"],
+
         ]
 
         # Create labels and add them to the reference list...
@@ -44,25 +40,22 @@ class StartGame:
         # error message if necessary.
         self.choose_label = start_label_ref[2]
 
-        # Frame so that entry box and button can be in the same row.
-        self.entry_area_frame = Frame(self.start_frame)
-        self.entry_area_frame.grid(row=3)
-
-        self.num_rounds_entry = Entry(self.entry_area_frame, font=("Arial", 20, "bold"),
+        self.num_rounds_entry = Entry(self.start_frame, font=("Arial", 20, "bold"),
                                       width=10)
-        self.num_rounds_entry.grid(row=0, column=0, padx=10, pady=10)
+        self.num_rounds_entry.grid(row=3, column=0, padx=10, pady=10)
 
-        self.num_rounds_infinite = Button(self.entry_area_frame, font=("Arial", 20, "bold"),
-                                          fg="#000000", bg="#DAE8FC", text="Infinite", width=10)
-        # self.num_rounds_infinite.grid(row=1, column=0, padx=10, pady=10)
+        self.num_rounds_infinite = Button(self.start_frame, font=("Arial", 20, "bold"),
+                                          fg="#000000", bg="#D2E2D3", text="Infinite", height=1)
+        self.num_rounds_infinite.grid(row=4, column=0, padx=10, pady=10)
 
         # Create play button
-        self.play_button = Button(self.entry_area_frame, font=("Arial", 20, "bold"),
+        self.play_button = Button(self.start_frame, font=("Arial", 20, "bold"),
                                   fg="#000000", bg="#D5E8D4", text="Play", width=10,
                                   command=self.check_rounds)
-        self.play_button.grid(row=1, column=0)
-        self.difficulty_button = Button(self.entry_area_frame, font=("Arial", 20, "bold"),
-                                        fg="#000000", bg="#F5F5F5", text="Difficulty", width=10)
+        self.play_button.grid(row=5, column=0)
+        self.difficulty_button = Button(self.start_frame, font=("Arial", 20, "bold"),
+                                        fg="#000000", bg="#D538D4", text="Difficulty", width=10)
+        self.difficulty_button.grid(row=6, column=0)
 
     def check_rounds(self):
         """
@@ -100,6 +93,6 @@ class StartGame:
 # main routine
 if __name__ == "__main__":
     root = Tk()
-    root.title("Colour Quest")
+    root.title("Country Flags")
     StartGame()
     root.mainloop()
